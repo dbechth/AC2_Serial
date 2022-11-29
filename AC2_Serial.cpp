ce3d9a2ab40f3af7685dd8f77f84d8386d80fbca
Doug Bechthold <dbechth@gmail.com>
2022-11-28 21:11:06 -0600

    Clean up

--- AC2_Serial.cpp
+++ AC2_Serial.cpp
@@ -1,177 +1,31 @@
 #include "AC2_Serial.h"
-#include <ArduinoOTA.h>
-#include "index.h" 
-#include "home.h" 
-#include "terminal.h"
 
 unsigned long taskRate;
 unsigned long taskCount;
 
-void handleTerminal()
-{
-	String tmpBuffer = "";
-	if (AC2_Serial.bufferCount != 0)
-	{
-		for (int i = 0; i < (AC2_Serial.bufferCount); i++)
-		{
-			tmpBuffer += AC2_Serial.buffer[i];
-		}
-		AC2_Serial.bufferCount = 0;
-	}
-	AC2_Serial.webserver.send(200, "text/plain", tmpBuffer);
-}
-
-String BuildJSON(String paramName, String Value)
-{
-	return "\"" + paramName + "\":\"" + Value + "\"";
-}
-
-void getData()
-{
-	String IOIndex = AC2_Serial.webserver.arg("IOIndex");
-	int idx = 0;
-	if (IOIndex != "")
-	{
-		idx = IOIndex.toInt();
-		if (idx > 15)
-		{
-			idx = 15;
-		}
-		if (idx < 0)
-		{
-			idx = 0;
-		}
-	}
-	AC2_SerialClass::IO tmpIO = AC2_Serial.device.Data[idx];
-
-	String tmpBuffer = "{ " +
-		BuildJSON("Name", tmpIO.Name) + ", " +
-		BuildJSON("Pin", String(tmpIO.Pin)) + ", " +
-		BuildJSON("Timeout", String(tmpIO.Timeout)) + ", " +
-		BuildJSON("DataTimer", String(tmpIO.DataTimer)) + ", " +
-		BuildJSON("Type", String(tmpIO.Type)) + ", " +
-		BuildJSON("DefaultValue", String(tmpIO.DefaultValue)) + ", " +
-		BuildJSON("Value", String(tmpIO.Value)) + ", " +
-		BuildJSON("Invert", String(tmpIO.Invert)) +
-		"}";
-
-	AC2_Serial.webserver.send(200, "text/plain", tmpBuffer);
-}
-
-void AC2_SerialClass::print(String message)
-{
-	if (bufferCount >= ACBufferSize)
-	{
-		buffer[0] = "Buffer Overflow! Messages were lost";
-		bufferCount = 1;
-	}
-	buffer[bufferCount] = message;
-	bufferCount++;
-}
-
-void AC2_SerialClass::println(String message)
-{
-	print(message + "<br>");
-}
-bool AC2_SerialClass::init(String name, IPAddress ip, IPAddress broadcastIP, int port, int TaskRateMS)
+bool AC2_SerialClass::init(String name, int TaskRateMS)
 {
 	device.Name = name;
-	device.IP = ip;
-	device.Port = port;
-	device.BroadcastIP = broadcastIP;//IPADDR_BROADCAST;
 	taskRate = TaskRateMS;
-	SetupOTA();
-
-	//webserver.on("/", AC2_SerialHome);      //Which routine to handle at root location. This is display page
-	webserver.on("/AC2_Serial", AC2Index);      //Which routine to handle at root location. This is display page
-	webserver.on("/getData", getData);
-	webserver.on("/terminal", terminal);      //Which routine to handle at root location. This is display page
-	webserver.on("/handleTerminal", handleTerminal);
-	webserver.begin();
 
 	bufferCount = 0;
-
 	//get started off right
 	timeNow = millis();
 	lastTime = timeNow;
-	return Udp.begin(device.Port);
+	return true;
 }
 
 
 void AC2_SerialClass::task()
 {
-
-	ArduinoOTA.handle();
-	webserver.handleClient();
-
 	timeNow = millis();
 	unsigned long elapsedTime = timeNow - lastTime;
 	if (elapsedTime >= taskRate) {
 		lastTime = timeNow;
 		ReadMessages();
 		HandleIO();
-		//WriteIO();
-		//SendMessages();
 		taskCount++;
 	}
-
-}
-
-void AC2_SerialClass::SetupOTA()
-{
-	// Port defaults to 8266
- // ArduinoOTA.setPort(8266);
-
- // Hostname defaults to esp8266-[ChipID]
-	ArduinoOTA.setHostname(device.Name.c_str());
-
-	// No authentication by default
-	// ArduinoOTA.setPassword("admin");
-
-	// Password can be set with it's md5 value as well
-	// MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-	// ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
-
-	ArduinoOTA.onStart([]() {
-		String type;
-		if (ArduinoOTA.getCommand() == U_FLASH) {
-			type = "sketch";
-		}
-		else { // U_FS
-			type = "filesystem";
-		}
-
-		// NOTE: if updating FS this would be the place to unmount FS using FS.end()
-		Serial.println("Start updating " + type);
-		});
-	ArduinoOTA.onEnd([]() {
-		Serial.println("\nEnd");
-		});
-	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-		Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-		});
-	ArduinoOTA.onError([](ota_error_t error) {
-		Serial.printf("Error[%u]: ", error);
-		if (error == OTA_AUTH_ERROR) {
-			Serial.println("Auth Failed");
-		}
-		else if (error == OTA_BEGIN_ERROR) {
-			Serial.println("Begin Failed");
-		}
-		else if (error == OTA_CONNECT_ERROR) {
-			Serial.println("Connect Failed");
-		}
-		else if (error == OTA_RECEIVE_ERROR) {
-			Serial.println("Receive Failed");
-		}
-		else if (error == OTA_END_ERROR) {
-			Serial.println("End Failed");
-		}
-		});
-	ArduinoOTA.begin();
-	Serial.println("Ready");
-	Serial.print("IP address: ");
-	Serial.println(WiFi.localIP());
 }
 
 void AC2_SerialClass::HandleIO()
@@ -210,25 +64,21 @@ void AC2_SerialClass::HandleIO()
 				device.Data[i].Value = digitalRead(device.Data[i].Pin) == 1;
 			}
 		}
-
 	}
 }
 
 void AC2_SerialClass::ReadMessages()
 {
-	int packetSize = Udp.parsePacket();
+	int packetSize = 10;
 	if (packetSize)
 	{
 		char incomingPacket[256];
-		// receive incoming UDP packets
-		Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
-		int len = Udp.read(incomingPacket, 255);
+		// receive incoming packets
+		int len = 10;
 		if (len > 0)
 		{
 			incomingPacket[len] = 0;
 
-			print(String(millis()) + " " + "UDP: ");
-			println(incomingPacket);
 			String packet = incomingPacket;
 			String packetRecipient = "";
 			if (packet.startsWith(device.Name))// ignore messages not sent to you and not broadcast
@@ -242,68 +92,23 @@ void AC2_SerialClass::ReadMessages()
 
 			if (packetRecipient.length() != 0)
 			{
-				//Serial.println(packet);
-
-				if (packet.startsWith(packetRecipient + ".WhoIs"))
+				if (packet.startsWith(packetRecipient + ".Write"))
 				{
-					//Serial.println(packet);
-					//HandleWhoIs(packet);
-				}
-				else if (packet.startsWith(packetRecipient + ".IAm"))
-				{
-					//Serial.println(packet);
-					//HandleIAm(packet);
-				}
-				else if (packet.startsWith(packetRecipient + ".Write"))
-				{
-					//Serial.println(packet);
 					HandleWriteCommand(packet);
 				}
-				else if (packet.startsWith(packetRecipient + ".Read"))
-				{
-					//Serial.println(packet);
-					HandleReadCommand(packet);
-				}
-				else if (packet.startsWith(packetRecipient + ".Reply"))
-				{
-					//Serial.println(packet);
-					HandleReplyCommand(packet);
-				}
-				else if (packet.startsWith(packetRecipient + ".Configure"))
-				{
-					//Serial.println(packet);
-					HandleConfigureCommand(packet);
-				}
 			}
-
 		}
 	}
 }
 
 void AC2_SerialClass::SendMessages()
 {
-	Udp.beginPacket(device.BroadcastIP, device.Port);
-	Udp.print(device.Name + "," + device.Data[0].Value + "," + device.Data[1].Value);
-	Udp.endPacket();
+	Serial.println(device.Name + "," + device.Data[0].Value + "," + device.Data[1].Value);
 }
 
 void AC2_SerialClass::WriteIO(String DeviceName, IO io)
 {
-
-	if (Udp.beginPacket(device.BroadcastIP, device.Port) == 1)
-	{
-		Udp.print(DeviceName + ".Write(" + io.Name + ")%" + io.Value + "%{" + device.Name + "}");
-		if (Udp.endPacket() == 0)
-		{
-			Serial.println("reply send failed");
-		}
-	}
-	else
-	{
-		Serial.println("beginPacket failed");
-	}
-
-
+	Serial.println(DeviceName + ".Write(" + io.Name + ")%" + io.Value + "%{" + device.Name + "}");
 }
 
 void AC2_SerialClass::LocalWriteIO(IO io)
@@ -337,12 +142,9 @@ void AC2_SerialClass::HandleWriteCommand(String command)
 			int valueStart = command.indexOf("%", 0) + 1;
 			int valueEnd = command.indexOf("%", valueStart);
 			String value = command.substring(valueStart, valueEnd);
-			//Serial.println(value);
+
 			device.Data[i].Value = value.toInt();
 			device.Data[i].DataTimer = device.Data[i].Timeout;
-			//Serial.print(device.Data[i].Name);
-			//Serial.print(": ");
-			//Serial.println(device.Data[i].DataTimer);
 		}
 	}
 }
